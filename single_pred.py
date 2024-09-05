24af21210353a0fb5370cb78e1d18b92c9d59c18
Zhao-Penghai <oceanytech@gmail.com>
2024-09-06 00:56:17 +0800

    240906

--- single_pred.py
+++ single_pred.py
@@ -1,46 +1,34 @@
-from transformers import pipeline
-import torch
-from datetime import datetime
-import torch
-from torch.nn.functional import mse_loss
-from torch.utils.data import Dataset, DataLoader
-from transformers import RobertaTokenizer, AdamW, RobertaForSequenceClassification, AutoTokenizer, \
-    AutoModelForSequenceClassification, FlaxLlamaForCausalLM, LlamaForSequenceClassification
-import pandas as pd
-from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data.dataset import random_split
-from tqdm import tqdm
-import os
-import argparse
-import json
-import torch
-from peft import PeftModel,PeftModelForTokenClassification
-import os
+
 import torch.nn as nn
-from tools.order_metrics import *
-import transformers.models.qwen2
-from peft import AutoPeftModelForCausalLM,AutoPeftModelForSequenceClassification,AutoPeftModelForTokenClassification
+
+from peft import AutoPeftModelForSequenceClassification
 from transformers import AutoTokenizer
-import torch
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, TaskType
-
-
-
-title = '''xxxx'''
-abstract = ''' xxx '''
 
 
 
 
-model_pth = r"xxx"
+
+
+
+
+
+model_pth = r"xxx" # Warning, you have to modify the "base_model_name_or_path" in adapter_config.json. We will fix this error after the rebuttal.
 model = AutoPeftModelForSequenceClassification.from_pretrained(model_pth,num_labels=1, load_in_8bit=True,)
 tokenizer = AutoTokenizer.from_pretrained(model_pth)
 
 model = model.to("cuda")
 model.eval()
-# Default Prompt Template
-text = f'''Given a certain paper, Title: {title}\n Abstract: {abstract}. \n Predict its normalized academic impact (between 0 and 1):'''
-inputs = tokenizer(text, return_tensors="pt")
 
-outputs = model(input_ids=inputs["input_ids"].to("cuda"))
-print(outputs['logits'])
+
+while True:
+    title = input("Enter a title: ")
+    abstract = input("Enter a abstract: ")
+    title = title.replace("\n", "").strip()
+    abstract = abstract.replace("\n", "").strip()
+    # Default Prompt Template
+    text = f'''Given a certain paper, Title: {title}\n Abstract: {abstract}. \n Predict its normalized academic impact (between 0 and 1):'''
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(input_ids=inputs["input_ids"].to("cuda"))
+    # If you haven't modify the LLaMA code.
+    print(nn.Sigmoid()(outputs['logits']))
+    # Else print(outputs['logits'])
