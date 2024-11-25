4d300b5fea9e076c6f8f87a75287d7132835719b
Zhao-Penghai <oceanytech@gmail.com>
2024-11-25 23:59:55 +0800

    241125

--- single_pred.py
+++ single_pred.py
@@ -1,14 +1,15 @@
-
 import torch.nn as nn
-
 from peft import AutoPeftModelForSequenceClassification
 from transformers import AutoTokenizer
+import torch
 
-model_pth = r"path_to_the_weights_dir" # Warning, you have to modify the "base_model_name_or_path" in adapter_config.json. We will fix this error after the rebuttal.
+
+# Warning, you have to modify the "base_model_name_or_path" in adapter_config.json. We will fix this error after the rebuttal.
+model_pth = r"path_to_the_weights_dir"
 model = AutoPeftModelForSequenceClassification.from_pretrained(model_pth,num_labels=1, load_in_8bit=True,)
 tokenizer = AutoTokenizer.from_pretrained(model_pth)
-
-model = model.to("cuda")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = model.to(device)
 model.eval()
 
 
@@ -17,10 +18,15 @@ while True:
     abstract = input("Enter a abstract: ")
     title = title.replace("\n", "").strip()
     abstract = abstract.replace("\n", "").strip()
+
     # Default Prompt Template
     text = f'''Given a certain paper, Title: {title}\n Abstract: {abstract}. \n Predict its normalized academic impact (between 0 and 1):'''
     inputs = tokenizer(text, return_tensors="pt")
     outputs = model(input_ids=inputs["input_ids"].to("cuda"))
+
+    
     # If you haven't modify the LLaMA code.
     print(nn.Sigmoid()(outputs['logits']))
     # Else print(outputs['logits'])
+
+
